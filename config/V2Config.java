86274bf52083ea855a8f7e195db7eeb2749f2928
hamza <hamzabenzidane01@gmail.com>
2026-08-16 15:18:43 +0100

    Update V2Config.java

--- config/V2Config.java
+++ config/V2Config.java
@@ -1,35 +1,18 @@
 package antonio.mexico.service.v2ray;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 
-public class V2Configs {
+public class V2Config implements Serializable {
 
-    public static final long BYTE = 1;
-    public static final long KILO_BYTE = BYTE * 1024;
-    public static final long MEGA_BYTE = KILO_BYTE * 1024;
-    public static final long GIGA_BYTE = MEGA_BYTE * 1024;
-    public static V2RAY_CONNECTION_MODES V2RAY_CONNECTION_MODE = V2RAY_CONNECTION_MODES.VPN_TUN;
-    public static String APPLICATION_NAME = null;
-    public static int APPLICATION_ICON = 0;
-    public static V2Config V2RAY_CONFIG = null;
-    public static V2RAY_STATES V2RAY_STATE = V2RAY_STATES.V2RAY_DISCONNECTED;
-    public static boolean ENABLE_TRAFFIC_AND_SPEED_STATICS = true;
-
-    public enum V2RAY_SERVICE_COMMANDS {
-        START_SERVICE,
-        STOP_SERVICE,
-        MEASURE_DELAY
-    }
-
-    public enum V2RAY_STATES {
-        V2RAY_CONNECTED,
-        V2RAY_DISCONNECTED,
-        V2RAY_CONNECTING
-    }
-
-    public enum V2RAY_CONNECTION_MODES {
-        VPN_TUN,
-        PROXY_ONLY
-    }
-
-
+    public String CONNECTED_V2RAY_SERVER_ADDRESS = "";
+    public String CONNECTED_V2RAY_SERVER_PORT = "";
+    public int LOCAL_SOCKS5_PORT = 10808;
+    public int LOCAL_HTTP_PORT = 10809;
+    public ArrayList<String> BLOCKED_APPS = null;
+    public String V2RAY_FULL_JSON_CONFIG = null;
+    public boolean ENABLE_TRAFFIC_STATICS = false;
+    public String REMARK = "";
+    public String APPLICATION_NAME;
+    public int APPLICATION_ICON;
 }
